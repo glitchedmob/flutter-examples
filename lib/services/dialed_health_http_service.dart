0106bc5869f9c4b8215f2a6b9e7e1b1091862112
Levi Zitting <me@levizitting.com>
2021-12-10 09:33:55 -0600

    Add implimentation for Http and Storage services

--- lib/services/dialed_health_http_service.dart
+++ lib/services/dialed_health_http_service.dart
@@ -1,4 +1,38 @@
-class DialedHealthHttpService {
+import 'dart:io';
 
+import 'package:http/http.dart' as http;
+
+
+// example usage
+// final _http = GetIt.I.get<DialedHealthHttpService>();
+// final res = await _http.post('/api/some-url');
+
+class DialedHealthHttpService extends http.BaseClient {
+  // Find some way to get this, maybe a public method that sets it's value
+  // that the auth service can use
+  String? _authToken;
+  final String _urlPrefix = 'https://some-base-url';
+  final http.Client _inner = http.Client();
+
+  String get _authHeader => _authToken == null ? '' : 'Bearer $_authToken';
+
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest incoming) {
+    final request = incoming as http.Request;
+
+    final modifiedRequest = http.Request(
+      request.method,
+      Uri.parse('$_urlPrefix${request.url.toString()}'),
+    );
+
+    modifiedRequest.headers.addAll(request.headers);
+    modifiedRequest.encoding = request.encoding;
+    modifiedRequest.bodyBytes = request.bodyBytes.toList();
+
+    if (_authHeader != '') {
+      modifiedRequest.headers[HttpHeaders.authorizationHeader] = _authHeader;
+    }
+
+    return _inner.send(modifiedRequest);
+  }
 }
-
--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,11 +1,78 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
 
 class StorageService {
-  Future<void> set(String key, dynamic value) async {
+  static const _fileName = 'storage.json';
 
-  }
+  Map<String, dynamic>? _cachedStorage;
 
   Future<dynamic> get(String key) async {
+    final storage = await _loadStorage();
 
+    return storage[key];
+  }
+
+  Future<void> remove(String key) async {
+    final storage = await _loadStorage();
+
+    storage.remove(key);
+
+    await _saveStorage(storage);
+  }
+
+  Future<void> set(String key, dynamic value) async {
+    final storage = await _loadStorage();
+
+    storage[key] = value;
+
+    await _saveStorage(storage);
+  }
+
+  Future<Map<String, dynamic>> _loadStorage() async {
+    if(_cachedStorage != null)  {
+      return _cachedStorage ?? {};
+    }
+
+    var storage = <String, dynamic>{};
+
+    final storageFile = await _getStorageFile();
+
+    if (!await storageFile.exists()) {
+      return storage;
+    }
+
+    try {
+      final jsonString = await storageFile.readAsString();
+
+      if(jsonString != '') {
+        storage = json.decode(jsonString);
+      }
+    } catch(err) {
+      // show the user some error message. Maybe with a toast popup
+    }
+
+    _cachedStorage = storage;
+
+    return storage;
+  }
+
+  Future<void> _saveStorage(Map<String, dynamic> storage) async {
+    final jsonString = json.encode(storage);
+    final storageFile = await _getStorageFile();
+
+    if (!await storageFile.exists()) {
+      await storageFile.create();
+    }
+
+    await storageFile.writeAsString(jsonString);
+
+    _cachedStorage = storage;
+  }
+
+  Future<File> _getStorageFile() async {
+    final dir = await getApplicationDocumentsDirectory();
+    return File('${dir.path}/$_fileName');
   }
 }
-
